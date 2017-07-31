b23f95012aface1b9801d6a78f7e61b86c98a0ce
SankalpUGMan <sankalp_saoji@rediffmail.com>
2017-07-31 17:46:22 +0530

    Created the Game

---
+++ Shoot.js
@@ -0,0 +1,15 @@
+﻿var projectile : Rigidbody;
+var speed = 1000;
+
+function Update ()
+	{
+		if (Input.GetButtonDown ("Fire1"))
+		{
+			clone = Instantiate(projectile, transform.position, transform.rotation);
+			clone.velocity = transform.TransformDirection(Vector3 (0, 0, speed));
+
+			Destroy (clone.GameObject, 15);
+		    GetComponent.<AudioSource>().Play();
+		    GetComponent.<ParticleSystem>().Play();
+	 	}
+	 }
